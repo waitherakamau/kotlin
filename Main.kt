6e99d95a48626c6124f3751ac4bb9de6c3a9881b
waitherakamau <effencekamau@gmail.com>
2022-02-12 15:23:07 +0300

    first commit

---
+++ Main.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var name = ("Effence")
+    println(name)
+
+    var age = 16
+    println(age)
+
+    var phonenumber = "0712960766"
+    println(phonenumber)
+
+    var weight = (56)
+    println (weight)
+
+    var isakenyan : Boolean = true
+    println (isakenyan)
+
+    var isnotakenyan : Boolean = false
+    println (isnotakenyan)
+
+}
